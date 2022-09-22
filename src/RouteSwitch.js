b73b61ee5a1869566f22305131913f2a66d36f2a
Tural Musaibov <tural.musaibov@gmail.com>
2022-09-22 22:37:20 +0400

    initial file structure

---
+++ src/RouteSwitch.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./components/App";
+import Profile from "./components/Profile";
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RouteSwitch
